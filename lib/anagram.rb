bb960a8110a112b5df20e70fa0318571c30b2ce7
Calvin McDowell <calvinmac40@hotmail.com>
2019-09-19 23:51:02 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,26 +1,8 @@
 # Your code goes here!
 class Anagram
-  
   attr_accessor :some_word
   
   def initialize(some_word)
     @some_word = some_word
   end
-  
-  def match=(some_word)
-    @some_word.detect?.match
-  end
-  
-  def self.match(some_word)
-    @some_word.each do |word|
-       word.detect.match?
-    end
-  end
-  
-  def match
-    @some_word
-  end
-  
-  
-  
 end
