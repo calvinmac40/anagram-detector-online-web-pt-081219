10766a3571420196629287b95b5ed52ced186ffb
Calvin McDowell <calvinmac40@hotmail.com>
2019-09-20 00:01:36 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,11 +2,16 @@
 class Anagram
   attr_accessor :some_word
   
-  def initialize(some_word)
-    @some_word = some_word
+  def initialize(word)
+    @word = word
   end
   
-  def self.match(some_word)
-    @some_word.detect.match
+  def match(array_of_words)
+    array_of_words.select do 
+      |element|
+      (@word.split("").sort) ==
+      (element.split("").sort)
+    end
+  end
   end
 end
